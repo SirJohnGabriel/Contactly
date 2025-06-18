43029e7ddc31bc36945432a2d5cde55b70c60239
SirJohnGabriel <j.g.pagtalunan14@gmail.com>
2025-06-18 15:22:38 +0800

    update

--- UI/Contactly.web/src/app/app.ts
+++ UI/Contactly.web/src/app/app.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Contact } from '../models/contact.models';
+import { AsyncPipe } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, FormsModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected title = 'Contactly.web';
+  http = inject(HttpClient);
+
+  contactsForm = new FormGroup({
+    name: new FormControl<string>(''),
+    email: new FormControl<string | null>(null),
+    phone: new FormControl<string>(''),
+    favorite: new FormControl<boolean>(false),
+  })
+
+  contacts$ = this.getContacts();
+
+  onFormSubmit() {
+    const addContactRequest = {
+      name: this.contactsForm.value.name,
+      email: this.contactsForm.value.email,
+      phone: this.contactsForm.value.phone,
+      favorite: this.contactsForm.value.favorite
+    }
+
+    this.http.post('https://localhost:7140/api/Contacts', addContactRequest)
+    .subscribe({
+      next: (value) => {
+        console.log(value);
+        this.contacts$ = this.getContacts();
+        this.contactsForm.reset()
+      }
+    });
+  }
+
+  private getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>('https://localhost:7140/api/Contacts');
+  }
 }
+
